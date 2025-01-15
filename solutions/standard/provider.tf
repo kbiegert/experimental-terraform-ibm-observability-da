ab3f13e4ff13f5c6cae7c9d66d067be6d71268c2
K. Biegert <67793262+kbiegert@users.noreply.github.com>
2025-01-15 16:30:31 -0500

    fix up provider file to not use local vars

--- solutions/standard/provider.tf
+++ solutions/standard/provider.tf
@@ -7,15 +7,15 @@ provider "ibm" {
   region           = var.region
 }
 
-provider "ibm" {
-  alias            = "cos"
-  ibmcloud_api_key = var.ibmcloud_cos_api_key != null ? var.ibmcloud_cos_api_key : var.ibmcloud_api_key
-  region           = local.default_cos_region
-}
+# provider "ibm" {
+#   alias            = "cos"
+#   ibmcloud_api_key = var.ibmcloud_cos_api_key != null ? var.ibmcloud_cos_api_key : var.ibmcloud_api_key
+#   region           = local.default_cos_region
+# }
 
 
-provider "ibm" {
-  alias            = "kms"
-  ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
-  region           = local.kms_region
-}
+# provider "ibm" {
+#   alias            = "kms"
+#   ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
+#   region           = local.kms_region
+# }
